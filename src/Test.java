eafc5098e6c8195d95fbb7cf8d413df9c7e6db8a
nares <nares@192.168.1.8>
2019-11-23 19:37:12 +0530

    Master updated

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Test {
+		
+	public static void main(String[] args) {
+		
+		ArrayList ArrayListlist = new ArrayList();
+		ArrayListlist.add(10);
+		ArrayListlist.add(20);
+		ArrayListlist.add(30);
+		ArrayListlist.add(15);
+		
+		System.out.println(ArrayListlist.get(0));
+	}
+
+}
